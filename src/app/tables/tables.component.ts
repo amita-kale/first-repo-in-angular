5b6ccba5bbf580539957b6727b37d767efb8de73
Amita kale <amitakale297@gmail.com>
2021-03-04 14:50:29 +0530

    correct code

---
+++ src/app/tables/tables.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tables-root',
+  templateUrl: './tables.component.html',
+  styleUrls: ['./tables.component.css']
+})
+export class TablesComponent {
+  title1 = 'first-repo-in-angular1';
+}
